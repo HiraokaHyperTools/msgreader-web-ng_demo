750dc4f2ab84e390705ddc9f7b86c1d95341a825
kenjiuno <ku@digitaldolphins.jp>
2026-02-27 10:54:30 +0900

    svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,6 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: "404.html",
-			pages: "docs",
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
