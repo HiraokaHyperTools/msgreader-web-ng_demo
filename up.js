77dde05c52e7d5ce87ac3e1b48fbca0320d43f9e
kenjiuno <ku@digitaldolphins.jp>
2026-02-27 11:10:23 +0900

    up.js

---
+++ up.js
@@ -0,0 +1,29 @@
+// for package maintenance: commit and push
+// usage: node up.js
+
+import { promisify } from 'util';
+import { promises } from 'fs';
+import child_process from 'child_process';
+const exec = promisify(child_process.exec);
+
+async function run() {
+  try {
+    await exec("robocopy build docs /xf .git", {});
+  }
+  catch (e) {
+    if (e.code !== 2) {
+      console.error("robocopy error", e.code);
+      return;
+    }
+  }
+  await promises.writeFile("docs/.nojekyll", "");
+
+  await exec("git add -A", { cwd: "docs" });
+  await exec("git commit -a -m \"Update doc\"", { cwd: "docs" });
+  await exec("git push", { cwd: "docs" });
+  await exec("git commit docs -m \"- Update doc\"", {});
+  await exec("git push", {});
+  console.info("OK!");
+}
+
+run();
